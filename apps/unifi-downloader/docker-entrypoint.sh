9ce57ae96a6afcddccd0f748c97fefd3fe6201ee
dmcavinue <dmcavinue@gmail.com>
2021-09-07 14:02:22 -0400

    add host output

--- apps/unifi-downloader/docker-entrypoint.sh
+++ apps/unifi-downloader/docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# UNIFI_HOST="192.168.1.1"
-# UNIFI_USER="unifi-downloader"
-# UNIFI_PWD="xxxxxxxx"
-# OUTPUT_DIR="/mnt/data/cctv"
-
 MINUTES=${MINUTES:-5}
 START_TIME=$(date -u -d "$date -${MINUTES} mins" +"%Y-%m-%dT%H:%M:%S")
 CURRENT_TIME=$(date -u -d "$date -0 mins" +"%Y-%m-%dT%H:%M:%S")
+
+echo "host:${UNIFI_HOST}"
 
 protect-archiver \
   events \
