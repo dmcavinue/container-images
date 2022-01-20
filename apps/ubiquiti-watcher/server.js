d4bbc940ee5dc8fd283258676c95c3aea2b3df4b
dmcavinue <dan@mcavinue.io>
2022-01-19 21:26:19 -0500

    fix ubiquiti watcher

--- apps/ubiquiti-watcher/server.js
+++ apps/ubiquiti-watcher/server.js
@@ -22,7 +22,7 @@ var mqttPassword = 'password';
 if(process.env.MQTT_PASSWORD) {
     mqttPassword = process.env.MQTT_PASSWORD;
 }
-var mqttTopic = '/ubiquiti-watcher';
+var mqttTopic = 'ubiquiti-watcher';
 if(process.env.MQTT_TOPIC) {
     mqttTopic = process.env.MQTT_TOPIC;
 }
@@ -47,14 +47,14 @@ if(process.env.DESIRED_PRODUCTS) {
 }
 
 // MQTT Client
-const client = mqtt.connect(mqttConnectUrl, {
+const mqttClient = mqtt.connect(mqttConnectUrl, {
   mqttClientId,
   clean: true,
   connectTimeout: 4000,
   username: mqttUsername,
   password: mqttPassword,
   reconnectPeriod: 1000,
-})
+});
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -73,15 +73,12 @@ const client = mqtt.connect(mqttConnectUrl, {
         });        
 
         var success = function(product){
-            console.log(util.format('%s/%s : %s', mqttTopic, product.toLowerCase(), Date.now()));
-            client.on('connect', () => {
-              client.publish(util.format('%s/products/%s', mqttTopic, product.toLowerCase()), Date.now(), { qos: 0, retain: false }, (error) => {
-                if (error) {
-                  console.error(error)
-                }
-              })
-            })
-            
+          console.log(util.format('%s/products/%s : %s', mqttTopic, product.toLowerCase(), Date.now().toString()));
+          mqttClient.publish(util.format('%s/products/%s', mqttTopic, product.toLowerCase()), Date.now().toString(), { qos: 0, retain: false }, (error) => {
+            if (error) {
+              console.error(error)
+            }
+          })
         }
         var functionToInject = function(){
             return document.querySelector("#app").__vue__.appData.cartAccessories.map(a=>a.title);
@@ -96,16 +93,14 @@ const client = mqtt.connect(mqttConnectUrl, {
         }
         console.log((foundItems.length?"":"Desired product(s) not found. ") + 
             "Waiting " +  toWait + "s to check again. ");
-        if(showAllAvailableFilters.length){
-          console.log("Available Products: ");
-          console.log(data.filter(a=>showAllAvailableFilters.some(b=>a.startsWith(b))));
-        }
-        client.on('connect', () => {
-          client.publish(util.format('%s/timestamp', mqttTopic), Date.now(), { qos: 0, retain: false }, (error) => {
-            if (error) {
-              console.error(error)
-            }
-          })
+        // if(showAllAvailableFilters.length){
+        //   console.log("Available Products: ");
+        //   console.log(data.filter(a=>showAllAvailableFilters.some(b=>a.startsWith(b))));
+        // }
+        mqttClient.publish(util.format('%s/timestamp', mqttTopic), Date.now().toString(), { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.error(error)
+          }
         })
         setTimeout(getAvailable, toWait*1000);
     }
